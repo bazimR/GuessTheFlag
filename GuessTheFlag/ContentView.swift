13c83fff514bebd1c08996291176b38d31384e11
bazimR <rishaalmuhammed@gmail.com>
2025-02-16 19:36:02 +0530

    helpfull msg when wrong

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -115,7 +115,7 @@ struct ContentView: View {
             showingTitle = "Correct answer!"
             scoreCount += 1
         } else {
-            showingTitle = "Wrong answer"
+            showingTitle = "Wrong answer, Thats the flag of \(countries[number])"
             userHearts -= 1
 
         }
